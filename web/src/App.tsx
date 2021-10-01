febbd989414b96e2bc7bf6c71edb5c9d275fdf1a
Bruno Futema <bruno.futema@outlook.com>
2021-10-01 19:45:31 -0300

    feat: :sparkles: Add basic structure of React application

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { GlobalStyles } from '@styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello React App</h1>
+      <GlobalStyles />
+    </>
+  );
+};
+
+export { App };
