a6290b55448ce64cd26a027681c595152d3274e5
1garo <alevardai427@gmail.com>
2021-10-19 19:53:43 -0300

    feat: improve users
    Modify response and remove useless fields on user table

--- server/src/modules/users/app/typeorm/entities/User.ts
+++ server/src/modules/users/app/typeorm/entities/User.ts
@@ -14,45 +14,15 @@ class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ length: 20 })
-  username: string;
-
   @Column({ length: 80 })
   name: string;
 
   @Column({ length: 80 })
   email: string;
 
-  @Column({ default: false })
-  @Exclude()
-  email_confirmed: boolean;
-
   @Column()
   @Exclude()
   password_hash: string;
-
-  @Column()
-  @Exclude()
-  security_stamp: string;
-
-  @Column({ default: false })
-  @Exclude()
-  two_factor_enabled: boolean;
-
-  @Column({ default: false })
-  @Exclude()
-  lockout_enabled: boolean;
-
-  @Column()
-  @Exclude()
-  lockout_expires: Date;
-
-  @Column({ default: true })
-  enabled: boolean;
-
-  @Column({ default: 0 })
-  @Exclude()
-  access_failed_count: number;
 
   @CreateDateColumn()
   created_at: Date;
